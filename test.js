2e1f2ac6ffbaf2dadd65843609779528719f2c7e
Tommaso Allevi <tomallevi@gmail.com>
2017-10-12 20:31:30 +0200

    Fix bad english

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var t = require('tap')
-const stringifyWrapper = require('./index')
+const factory = require('./index')
 
 t.test('object with string props', t => {
   var schema = {
@@ -11,9 +11,9 @@ t.test('object with string props', t => {
       bar: { type: 'string' }
     }
   }
-  var stringify = stringifyWrapper(schema)
+  var stringify = factory(schema)
 
-  var testes = [
+  var tests = [
     {
       input: {foo: 'value1', bar: 'value2'},
       output: 'foo=value1&bar=value2'
@@ -35,9 +35,9 @@ t.test('object with string props', t => {
       output: 'foo=&bar=value2'
     }
   ]
-  t.plan(testes.length)
+  t.plan(tests.length)
 
-  testes.forEach(test => {
+  tests.forEach(test => {
     t.test(JSON.stringify(test.input), t => {
       t.plan(1)
       var encoded = stringify(test.input)
@@ -54,9 +54,9 @@ t.test('object with string and int props', t => {
       bar: { type: 'integer' }
     }
   }
-  var stringify = stringifyWrapper(schema)
+  var stringify = factory(schema)
 
-  var testes = [
+  var tests = [
     {
       input: {foo: 'value1', bar: '33'},
       output: 'foo=value1&bar=33'
@@ -74,9 +74,9 @@ t.test('object with string and int props', t => {
       output: 'foo=value1'
     }
   ]
-  t.plan(testes.length)
+  t.plan(tests.length)
 
-  testes.forEach(test => {
+  tests.forEach(test => {
     t.test(JSON.stringify(test.input), t => {
       t.plan(1)
       var encoded = stringify(test.input)
@@ -92,9 +92,9 @@ t.test('object with boolean props', t => {
       foo: { type: 'boolean' }
     }
   }
-  var stringify = stringifyWrapper(schema)
+  var stringify = factory(schema)
 
-  var testes = [
+  var tests = [
     {
       input: { foo: true },
       output: 'foo=true'
@@ -120,9 +120,9 @@ t.test('object with boolean props', t => {
       output: ''
     }
   ]
-  t.plan(testes.length)
+  t.plan(tests.length)
 
-  testes.forEach(test => {
+  tests.forEach(test => {
     t.test(JSON.stringify(test.input), t => {
       t.plan(1)
       var encoded = stringify(test.input)
@@ -138,9 +138,9 @@ t.test('object with null props', t => {
       foo: { type: 'null' }
     }
   }
-  var stringify = stringifyWrapper(schema)
+  var stringify = factory(schema)
 
-  var testes = [
+  var tests = [
     {
       input: { foo: true },
       output: 'foo='
@@ -166,9 +166,9 @@ t.test('object with null props', t => {
       output: ''
     }
   ]
-  t.plan(testes.length)
+  t.plan(tests.length)
 
-  testes.forEach(test => {
+  tests.forEach(test => {
     t.test(JSON.stringify(test.input), t => {
       t.plan(1)
       var encoded = stringify(test.input)
@@ -191,9 +191,9 @@ t.test('object with object props', t => {
       }
     }
   }
-  var stringify = stringifyWrapper(schema)
+  var stringify = factory(schema)
 
-  var testes = [
+  var tests = [
     {
       input: { foo: { key1: 'qq', key2: '44', key3: true } },
       output: 'foo[key1]=qq&foo[key2]=44&foo[key3]=true'
@@ -235,9 +235,9 @@ t.test('object with object props', t => {
       output: ''
     }
   ]
-  t.plan(testes.length)
+  t.plan(tests.length)
 
-  testes.forEach(test => {
+  tests.forEach(test => {
     t.test(JSON.stringify(test.input), t => {
       t.plan(1)
       var encoded = stringify(test.input)
@@ -256,9 +256,9 @@ t.test('array of string', t => {
       }
     }
   }
-  var stringify = stringifyWrapper(schema)
+  var stringify = factory(schema)
 
-  var testes = [
+  var tests = [
     {
       input: { foo: ['q', 'w', 'e', 'r', 't', 'y'] },
       output: 'foo[]=q&foo[]=w&foo[]=e&foo[]=r&foo[]=t&foo[]=y'
@@ -284,9 +284,9 @@ t.test('array of string', t => {
       output: 'foo[]=55&foo[]=&foo[]=iiiii&foo[]=1970-01-01T00:00:00.000Z'
     }
   ]
-  t.plan(testes.length)
+  t.plan(tests.length)
 
-  testes.forEach(test => {
+  tests.forEach(test => {
     t.test(JSON.stringify(test.input), t => {
       t.plan(1)
       var encoded = stringify(test.input)
@@ -295,8 +295,8 @@ t.test('array of string', t => {
   })
 })
 
-t.test('fail build serializator', t => {
-  var testes = [
+t.test('fail build serializer', t => {
+  var tests = [
     {
       schema: { type: 'foobar' }
     },
@@ -307,13 +307,13 @@ t.test('fail build serializator', t => {
       schema: { type: 'array', items: { type: 'foobar' } }
     }
   ]
-  t.plan(testes.length)
+  t.plan(tests.length)
 
-  testes.forEach(test => {
+  tests.forEach(test => {
     t.test(JSON.stringify(test.schema), t => {
       t.plan(1)
       try {
-        stringifyWrapper(test.schema)
+        factory(test.schema)
         t.fail()
       } catch (e) {
         t.ok(/^Unable /.test(e.message), e.message)
